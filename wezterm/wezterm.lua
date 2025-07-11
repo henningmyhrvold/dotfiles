873d97f896ecbdb535eaad97e5df47f0b18ec1d4
Henning Myhrvold <henning.myhrvold@gmail.com>
2025-07-11 22:08:24 +0200

    new features

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,129 +1,98 @@
--- File template from
--- https://wezfurlong.org/wezterm/config/files.html#configuration-file-structure
-
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
--- This table will hold the configuration.
-local config = {}
+-- local emit = wezterm.emit
+-- wezterm.emit = function(a, ...)
+--     print("emit", a)
+--     emit(wezterm, a, ...)
+-- end
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
+local function log_proc(proc, indent)
+  indent = indent or ''
+  wezterm.log_info(
+    indent
+      .. 'pid='
+      .. proc.pid
+      .. ', name='
+      .. proc.name
+      .. ', status='
+      .. proc.status
+  )
+  wezterm.log_info(indent .. 'argv=' .. table.concat(proc.argv, ' '))
+  wezterm.log_info(
+    indent .. 'executable=' .. proc.executable .. ', cwd=' .. proc.cwd
+  )
+  for _, child in pairs(proc.children) do
+    log_proc(child, indent .. '  ')
+  end
 end
 
--- ====================================================
--- ====================================================
--- This is where you actually apply your config choices
+wezterm.on('mux-is-process-stateful', function(proc)
+  log_proc(proc)
 
--- For example, changing the color scheme:
--- config.color_scheme = 'AdventureTime'
-
--- On Windows, add PowerShell and pwsh to launcher, set default shell
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-	 config.default_prog = { 'nu.exe' }
-	 config.launch_menu = {}
-	 table.insert(config.launch_menu, { label = 'PowerShell -NoProfile', args = {'powershell.exe', '-NoProfile', '-NoLogo'},
-	 })
-	 table.insert(config.launch_menu, { label = 'PowerShell', args = {'powershell.exe', '-NoLogo'},
-	 })
-   table.insert(config.launch_menu, { label = 'pwsh -NoProfile', args = {'pwsh.exe', '-NoProfile'},
-	 })
-	 table.insert(config.launch_menu, { label = 'pwsh', args = {'pwsh.exe'},
-	 })
-	 table.insert(config.launch_menu, { label = 'nushell', args = {'nu.exe'},
-	 })
-	 table.insert(config.launch_menu, { label = 'cmd', args = {'cmd.exe'},
-	 })
-	 table.insert(config.launch_menu, { label = 'msys2-crt64', args = {'ucrt64.cmd'},
-	 })
-	 table.insert(config.launch_menu, { label = 'Cygwin Shell', args = {'cygwin-shell.bat'},
-	 })
-
-
-end
-
--- On Linux, add bash and nushell to launch, set default shell
-if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
-	 config.default_prog = { 'bash' }
-	 config.launch_menu = {}
-	 table.insert(config.launch_menu, { label = 'bash', args = {'bash'},
-	 })
-	 table.insert(config.launch_menu, { label = 'nushell', args = {'nu'},
-	 })
-	 table.insert(config.launch_menu, { label = 'fish', args = {'fish'},
-	 })
-end
-
--- Enable the scrollbar.
--- It will occupy the right window padding space.
--- If right padding is set to 0 then it will be increased
--- to a single cell width
-config.enable_scroll_bar = true
-
-config.keys = {
-	-- Override ToogleFullScreen keybinding to F11
-	-- Turn off / Disable default alt-enter action 
-  -- allowing it to be recognized and handled by the tab
-  {
-    key = 'Enter',
-    mods = 'ALT',
-    action = wezterm.action.DisableDefaultAssignment,
-  },
-	{
-		key = "F11",
-    action = wezterm.action.ToggleFullScreen,
-  },
-
-}
-
--- Saving scrollback and pane
--- from: https://wezfurlong.org/wezterm/config/lua/pane/get_lines_as_text.html
-
-local io = require 'io'
-local os = require 'os'
-local act = wezterm.action
-
-wezterm.on('trigger-vim-with-scrollback', function(window, pane)
-  -- Retrieve the text from the pane
-  local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
-
-  -- Create a temporary file to pass to vim
-  local name = os.tmpname()
-  local f = io.open(name, 'w+')
-  f:write(text)
-  f:flush()
-  f:close()
-
-  -- Open a new window running vim and tell it to open the file
-  -- window:perform_action(
-  --   act.SpawnCommandInNewWindow {
-  --     args = { 'vim', name },
-  --   },
-  --   pane
-  -- )	
-
-  -- output the filename's location to the current active pane
-  pane:inject_output('Session saved to: ' .. name)
-
-  -- Wait "enough" time for vim to read the file before we remove it.
-  -- The window creation and process spawn are asynchronous wrt. running
-  -- this script and are not awaitable, so we just pick a number.
-  --
-  -- Note: We don't strictly need to remove this file, but it is nice
-  -- to avoid cluttering up the temporary directory.
-  -- wezterm.sleep_ms(1000)
-  -- os.remove(name)
+  -- Just use the default behavior
+  return nil
 end)
 
--- Merge the keys table with the existing one
-table.insert(config.keys, {
-  key = 'E',
-  mods = 'CTRL',
-  action = act.EmitEvent 'trigger-vim-with-scrollback',
-})
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  -- CTRL-SHIFT-l activates the debug overlay
+  --{ key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
+  --{ key = 'f', mods = 'CTRL', action = wezterm.action_callback(function(win, pane)
+  --      print("from within the action callback")
+  --  end)
+  --  },
+  --{ key = '', mods = 'CTRL', action = wezterm.action_callback(function(win, pane)
+  --      print("from within the action callback")
+  --  end)
+  --  },
+  {
+    key = "a",
+    mods = "LEADER|CTRL",
+    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+  },
 
--- ====================================================
--- and finally, return the configuration to wezterm
+  {
+    key = "v",
+    mods = "LEADER",
+    action = wezterm.action.SwitchToWorkspace {
+      spawn = {
+                cwd = "/home/theprimeagen/personal/dev",
+        args = { "nvim", "/home/theprimeagen/personal/dev" },
+      },
+    },
+  },
+
+    {
+        key = "w",
+        mods = "LEADER",
+        action = wezterm.action_callback(function(window, pane)
+            wezterm.log_error("wezterm.action.callback start")
+            window:perform_action(wezterm.action.SwitchToWorkspace {
+                spawn = {
+                    cwd = "/home/theprimeagen/personal/dev",
+                    args = { "nvim", "/home/theprimeagen/personal/dev/env/.config/wezterm/wezterm.lua" },
+                },
+            }, pane)
+            wezterm.log_error("wezterm.action.callback done")
+        end)
+    },
+}
+
+config.unix_domains = {
+  {
+    name = "unix",
+  },
+}
+
+-- This causes `wezterm` to act as though it was started as
+-- `wezterm connect unix` by default, connecting to the unix
+-- domain on startup.
+-- If you prefer to connect manually, leave out this line.
+config.default_gui_startup_args = { "connect", "unix" }
+
+wezterm.log_error("FRESH CONFIG LOAD!! 11")
+
+-- Your configurations go here
+config.font = wezterm.font 'JetBrains Mono'
 return config
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,32 +1,20 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 # If you come from bash you might have to change your $PATH.
- export PATH=$HOME/.local/bin:$PATH
-# export PATH="$HOME/.local/lib/python3.10/sitepackages/:$PATH"
-# export PATH="$HOME/tools/llvm-project/build/bin:$PATH"
-# export LD_LIBRARY_PATH="$HOME/tools/llvm-project/build/lib:$LD_LIBRARY_PATH"
- export EDITOR=nvim
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+
 # Path to your oh-my-zsh installation.
-ZSH=/usr/share/oh-my-zsh/
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-
-alias ls='lsd -a'
+ZSH_THEME="robbyrussell"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
 # a theme from this variable instead of looking in $ZSH/themes/
 # If set to an empty array, this variable will have no effect.
 # ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
@@ -82,8 +70,9 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(colorize)
+plugins=(git asdf)
 
+source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
@@ -110,36 +99,18 @@ plugins=(colorize)
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+source ~/.zsh_profile
 
-# Basic auto/tab complete:
-autoload -U compinit
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-compinit
-_comp_options+=(globdots)		# Include hidden files.
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
-bindkey ";3C" forward-word
-bindkey ";3D" backward-word
+# Bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
 
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-setopt appendhistory
-
-ZSH_CACHE_DIR=$HOME/.cache/oh-my-zsh
-if [[ ! -d $ZSH_CACHE_DIR ]]; then
-  mkdir $ZSH_CACHE_DIR
-fi
-
-# eval "$(starship init zsh)"
-
-source $ZSH/oh-my-zsh.sh
-source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-#eval "$(starship init zsh)"
-
-# Load syntax highlighting; should be last.
-#source /usr/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh 2>/dev/null
-
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end
+# Turso
+export PATH="$HOME/.turso:$PATH"
